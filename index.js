904287f771b70591c8827e1bd3a3aee649f28191
Asis <127653362+EKkoech@users.noreply.github.com>
2024-12-20 08:06:16 +0300

    added ejs and worked on external api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2');
 const path = require('path');
 const bcrypt = require('bcrypt'); 
 const dotenv = require('dotenv');
+const ejs = require('ejs');
+
 
 // Start app
 const app = express();
@@ -12,26 +14,33 @@ const app = express();
 dotenv.config();
 
 // Start database connection
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+// const db = mysql.createPool({
+//     connectionLimit:10,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 
-
+ 
 // Connect to database
-db.connect((err) => {
-    if (err) {
-        console.error('Error accessing database:', err.stack);
-        return;
-    }
-    console.log('Connected to database, thread ID:', db.threadId);
-});
+// let speciesdb = {}
+// speciesdb.all = ()=>{
+//     return new Promise((path.resolve, reject)=>(
+//         pool.query('SELECT * from dataset')
+//     ))
+// }
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Error accessing database:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to database, thread ID:', db.threadId);
+// });
 
 // Set view engine
 app.set('view engine', 'ejs');
-app.set('views', __dirname, '/views');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.json());
@@ -42,21 +51,40 @@ app.use(express.static(path.join(__dirname, 'Mazigira-Tracker')));
 
 //routes
 app.get('/', (req, res)=>{
-    res.sendfile('index.html')
+    res.render('index.ejs')
 });
 
 app.get('/species', (req, res)=> {
-    const speciesPath= path.resolve(__dirname,'species.html')
+    const speciesPath= path.resolve(__dirname,'views','index.ejs')
     res.sendFile(speciesPath);
 })
 app.get('/species1', async (req, res)=>{
 try {
     const response=await fetch('https://api.gbif.org/v1/occurrence/search?limit=10');
-    const biodata= await response.json();
-    res.json(biodata);
+    const results= await response.json();
+    const biodata = results.results;
+    const extractedData = biodata.map((item)=>(
+        {
+        scientificName: item.scientificName,
+        species: item.species,
+        genericName: item.genericName,
+        dateIdentified: item.dateIdentified,
+        decimalLatitude: item.decimalLatitude,
+        decimalLongitude: item.decimalLongitude,
+        continent: item.continent,
+        stateProvince: item.stateProvince,
+        eventDate: item.eventDate,
+        verbatimLocality:item.verbatimLocality,
+        mediaUrl: (item.media || []).find(media =>{
+            media.type === 'StillImage'
+        })?.identifier || 'No URL available'
+ 
+    }))
+    // res.json(extractedData);
+    res.render('index.ejs', {data: extractedData})
     }
-catch(err){
-    console.err("Error fetching biodata")
+catch(error){
+    console.error("Error fetching biodata", error)
     res.status(500).json({error:'Error fetching the biodata'})
 }})
 
