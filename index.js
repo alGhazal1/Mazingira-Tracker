c27e90f05f722c85c1531a762f8346dacee21dc9
seeker <seeker@kali.seeker.local>
2025-03-22 07:39:17 +0300

    work in progress. fixed /species route

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ app.get('/', (req, res)=>{
 app.get('/getStarted', (req, res) =>{
     res.redirect('/species')
 })
-app.get('/species', (req, res)=> {
-    res.render('local');
-})
 
 app.get('/user', (req, res)=> {
     res.render('user_home');
@@ -39,12 +36,14 @@ app.get('/user', (req, res)=> {
 
 app.get('/species', async (req, res)=>{
 try {
+   // const latitude = 0.1
+   // const longitude = 37.5
     const latitude = req.query.latitude;
     const longitude = req.query.longitude;
     console.log(req.query.latitude)
     console.log(req.query.longitude)
     if (!latitude || !longitude) {
-    res.status(400).json({error: 'latitude and longitude required'})
+    return res.status(400).json({error: 'latitude and longitude required'})
     };
 
     const response = await fetch(`https://api.gbif.org/v1/occurrence/search?decimalLatitude=${latitude}&decimalLongitude=${longitude}&within=100`);
@@ -71,17 +70,17 @@ try {
     }));
 
     console.log(extractedData)
-    res.render('local', {data: extractedData});
+    return res.render('local', {data: extractedData});
     }
 catch(error){
     console.error("Error fetching biodata", error)
     console.log(error);
-    res.status(500).json({error:'Error fetching the biodata'})
+    return res.status(500).json({error:'Error fetching the biodata'})
 }})
 
 // Start the server
 const port = process.env.PORT || 5001 
 app.listen(port, () => {
-    console.log('Server running on port: ${port}');
+    console.log('Server running on port: port');
 });
 
